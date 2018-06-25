015878d514edba1e6d37b78d61ccd7288b85806a
Jonathan Sosin <jonysosin@gmail.com>
2018-06-25 00:42:56 -0300

    Start

---
+++ index.js
@@ -0,0 +1,19 @@
+const router = require('koa-route');
+const mount = require('koa-mount');
+const auth = require('koa-basic-auth');
+const koa = require('koa');
+const config = require('config');
+const bodyparser = require('koa-bodyparser');
+const controller = require('./src/controller');
+
+let app = new koa();
+app.use(bodyparser());
+
+app.use(router.get("/api", () => {
+    ctx.body = 'Hola'
+}));
+
+if (!module.parent) {
+  app.listen(3131);
+  console.log("Listening on port 3131.");
+}
